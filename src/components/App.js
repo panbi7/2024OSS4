f61f45ab525329003387e0c6485a30ba3b7170b5
panbi7 <panbi7@naver.com>
2024-09-27 10:44:34 +0900

    add components

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header';
+
+function App() {
+    return (
+        <div className="App">
+            <Header />
+            <p>This is the main content of the site.</p>
+        </div>
+    );
+}
+
+export default App;
